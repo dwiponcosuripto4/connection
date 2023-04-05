10239f8ec3cc8791ac3134580b2fdf0da641bfd8
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-04-05 09:50:37 +0700

    Add project files.

---
+++ connection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().koneksi();
+        }
+        public void koneksi()
+        {
+            using (
+                //membuat koneksi
+                SqlConnection conn = new SqlConnection("data source=LAPTOP-9OD41I73\\DWIPONCOS;"
+                + "database=PerizinanKuliah;user ID=sa;password=xm11tpro"))
+            {
+                conn.Open();
+                Console.Write("Koneksi Sukses dengan Database");
+                Console.Read();
+            }
+
+        }
+        public void table()
+        {
+            using (SqlConnection connection = new SqlConnection($"Data Source=LAPTOP-9OD41I73\\DWIPONCOS;Initial Catalog=PerizinanKuliah;Integrated Security=True"))
+            {
+            }
+        }
+    }
+}
